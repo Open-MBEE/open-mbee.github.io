6f5718f3fc68dcf036abb562968fb2359c05deb7
Lam, Doris T (319E) <Doris.T.Lam@jpl.nasa.gov>
2020-04-14 16:58:54 -0700

    fix hash

--- js/tabs.js
+++ js/tabs.js
@@ -1,15 +1,19 @@
 // LINK TO TABS
-var url = window.location.href;
+var origurl = window.location.href;
 window.location.hash = "";
-function hashchanged() {
+function hashchanged(e, url) {
+  if (!url) {
+    url = window.location.href;
+  }
   if (url.indexOf("#") > 0){
   var activeTab = url.substring(url.indexOf("#") + 1);
     $('.nav[role="tablist"] a[href="#'+activeTab+'"]').tab('show');
   }
 }
 $(document).ready(() => {
-  hashchanged();
+  hashchanged(null, origurl);
   $('a[role="tab"]').on("click", function() {
+    var url = window.location.href;
     var newUrl;
     const hash = $(this).attr("href");
       newUrl = url.split("#")[0] + hash;
